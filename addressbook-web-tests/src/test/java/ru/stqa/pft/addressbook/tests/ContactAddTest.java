cbf7216e3c1edc70e079e480d70cb6aecc8261b4
kuaniysh <kuaniysh@gmail.com>
2016-04-01 02:29:04 +0600

    Задание №15

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddTest.java
@@ -1,34 +1,50 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactAddTest extends TestBase {
 
-    @Test
-    public void testAddContact() {
-        File photo = new File("src/test/resources/load.jpeg");
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test(dataProvider = "validContacts")
+    public void testAddContact(ContactData contact) {
+        app.goTo().homePage();
 
         Contacts before = app.contact().all();
-        ContactData contactData = new ContactData()
-                .withFirstname("TestName")
-                .withMiddleName("MiddleName")
-                .withLastName("LastName")
-                .withNickName("NickName")
-                .withMobilePhone("767854345")
-                .withGroup("test1")
-                .withPhoto(photo);
-        app.contact().createContact(contactData, true);
+        app.contact().createContact(contact, true);
         app.goTo().homePage();
         Contacts after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
 
-        assertThat(after, equalTo(before.withAdded(contactData.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 }
